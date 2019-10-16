deb861d899a6c2d5dc334c6537faaff587b2edd6
dduportal <1522731+dduportal@users.noreply.github.com>
2019-10-16 11:45:17 +0200

    Init adoc CV
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>

--- src/ruby/gen-css.rb
+++
@@ -1,9 +0,0 @@
-
-require 'sass'
-require 'fileutils'
-
-sass_engine = Sass::Engine.for_file('sass/base.scss', {})
-output = sass_engine.render
-File.open('export/styles/main.css', 'w') { |file|
-    file.write output
-}
